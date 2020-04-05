7f8a9ace8342534ae950cacc6340deacbdc12705
Nicolas Rodriguez <nicoladmin@free.fr>
2020-04-05 03:31:40 +0200

    Fix uninitialized constant RSpec::Mocks::ExampleMethods error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,13 +21,13 @@ require 'sidekiq/api'
 require 'timecop'
 require 'pry'
 
-# Requires supporting ruby files with custom matchers and macros, etc, in
-# spec/support/ and its subdirectories.
-Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
-
 # Load our own config
 require_relative 'config_rspec'
 require_relative 'config_capybara'
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories.
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
 
 def test_request
   if Rails.version >= '5.1'
